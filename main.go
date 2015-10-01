a82d2883f6f548765811076c70667fa5006ca167
Caleb Thompson <caleb@calebthompson.io>
2015-09-30 19:11:01 -0500

    Add version and help arguments
    `-v / --version` will print version information and exit.
    `-h / --help` will print help information and exit.
    
    Any other flag will complain and exit with a failure code.

--- main.go
+++ main.go
@@ -2,12 +2,24 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
 
 	"github.com/calebthompson/ftree/tree"
 )
 
+const (
+	version = "v0.1.0"
+	help    = `Pipe output from some command that lists a single file per line to ftree.
+	find . | ftree
+	git ls-files {app,spec}/models | ftree
+	lsrc | cut -d : -f 1 | ftree
+`
+)
+
 func main() {
+	handleArguments()
+
 	r := bufio.NewScanner(os.Stdin)
 	lines := []string{}
 	for r.Scan() {
@@ -16,4 +28,20 @@ func main() {
 
 	t := tree.Tree(lines, "/")
 	t.Print(0, nil)
+}
+
+func handleArguments() {
+	for _, a := range os.Args[1:] {
+		switch a {
+		case "--version", "-v":
+			fmt.Printf("ftree %s\n", version)
+			os.Exit(0)
+		case "--help", "-h":
+			fmt.Print(help)
+			os.Exit(0)
+		default:
+			fmt.Printf("No such argument: %v\n", a)
+			os.Exit(1)
+		}
+	}
 }
